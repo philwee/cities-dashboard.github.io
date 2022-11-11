cf78cff00a80a585d2d30e147c812bdec5687260
princeampofo <pla252@nyu.edu>
2022-11-11 20:24:50 +0400

    about page formatted

--- frontend/src/Pages/About/About.jsx
+++ frontend/src/Pages/About/About.jsx
@@ -14,16 +14,36 @@ export default function About() {
         <div className="aboutPageLogo">
           <img className="citiesPic" src={citiesPic} alt="" />
           <div className="contentIcons">
-            <a className="iconLink" href="https://twitter.com/cities_nyuad/" target="_blank" rel="noreferrer">
+            <a
+              className="iconLink"
+              href="https://twitter.com/cities_nyuad/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <AiFillTwitterCircle size={40} />
             </a>
-            <a className="iconLink" href="https://www.linkedin.com/company/center-for-interacting-urban-networks/" target="_blank" rel="noreferrer">
+            <a
+              className="iconLink"
+              href="https://www.linkedin.com/company/center-for-interacting-urban-networks/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <AiFillLinkedin size={40} />
             </a>
-            <a className="iconLink" href="https://www.facebook.com/nyuad.cities/" target="_blank" rel="noreferrer">
+            <a
+              className="iconLink"
+              href="https://www.facebook.com/nyuad.cities/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <BsFacebook size={35} />
             </a>
-            <a className="iconLink" href="https://www.instagram.com/cities.nyuad/" target="_blank" rel="noreferrer">
+            <a
+              className="iconLink"
+              href="https://www.instagram.com/cities.nyuad/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <AiFillInstagram size={40} />
             </a>
           </div>
@@ -32,7 +52,12 @@ export default function About() {
           <div className="infoText">
             <h4 className="aboutHeader">THE CENTER</h4>
             <div className="aboutText">
-              The NYUAD&apos;s Center for Ineracting Urban Networks (CITIES) is an interdisciplinary research center dedicated to advance urban science and promote cutting-edge research that is translated into practical, real-world solutions for the benefit of society. Our ultimate goal is to foster sustainable, resilient, and equitable cities.
+              The NYUAD&apos;s Center for Ineracting Urban Networks (CITIES) is
+              an interdisciplinary research center dedicated to advance urban
+              science and promote cutting-edge research that is translated into
+              practical, real-world solutions for the benefit of society. Our
+              ultimate goal is to foster sustainable, resilient, and equitable
+              cities.
             </div>
             <h4 className="aboutHeader">THE PROJECT</h4>
             <div className="aboutText">

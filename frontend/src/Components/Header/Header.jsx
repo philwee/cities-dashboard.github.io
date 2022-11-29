295a6b5945ca7bc708357a134bfb7afe18b1480a
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2022-11-29 15:39:55 +0700

    fixed header spacing

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -28,32 +28,33 @@ export default function Header({ LinkChange, setLinkChange }) {
   return (
     <Paper elevation={0} square>
       <Box sx={{ backgroundColor: 'primary.light', height: '10vh' }} />
-      <Container>
+      <Container sx={{pb: 3}}>
         <Paper elevation={4} sx={{ width: '12vh', height: '12vh', ml: 0, mt: '-6vh', mb: 3 }}>
           <img style={{ width: '100%' }} src={citiesLogo} />
         </Paper>
-        <Box>
+        <Box sx={{ mb: 3 }}>
           <Typography variant="h3" sx={{ fontWeight: 'medium' }}>CITIES DASHBOARD</Typography>
-          <Typography variant="body1" gutterBottom >Spearheading sustainability and well-being data visualization at NYU Abu Dhabi</Typography>
+          <Typography variant="body1">Spearheading sustainability and well-being data visualization at NYU Abu Dhabi</Typography>
         </Box>
-        <Container>
-          <Grid container>
-            {LinkChange ? (
-              <Link to="/">
-                <Typography className={underlineItem === 1 ? 'navLink active' : 'navLink'}
-                  onClick={() => underlineFunc(1)} variant="body1" sx={{ fontWeight: 'medium' }}>HOME</Typography>
-              </Link>
-            ) : (
-              <Link to="/" onClick={changeLinkContent}>
-                <Typography onClick={() => underlineFunc(1)} variant="body1" sx={{ fontWeight: 'medium' }}>HOME</Typography>
-              </Link>
-            )}
-            <Link to="/about">
-              <Typography className={underlineItem === 2 ? 'navLink active' : 'navLink'}
-                onClick={() => underlineFunc(2)} variant="body1" sx={{ fontWeight: 'medium' }}>ABOUT</Typography>
+
+        <Box sx={{display: 'inline-block', mr: 2}}>
+          {LinkChange ? (
+            <Link to="/">
+              <Typography className={underlineItem === 1 ? 'navLink active' : 'navLink'}
+                onClick={() => underlineFunc(1)} variant="body1" sx={{ fontWeight: 'medium' }}>HOME</Typography>
             </Link>
-          </Grid>
-        </Container>
+          ) : (
+            <Link to="/" onClick={changeLinkContent}>
+              <Typography onClick={() => underlineFunc(1)} variant="body1" sx={{ fontWeight: 'medium' }}>HOME</Typography>
+            </Link>
+          )}
+        </Box>
+        <Box sx={{display: 'inline-block', mr: 2}}>
+          <Link to="/about">
+            <Typography className={underlineItem === 2 ? 'navLink active' : 'navLink'}
+              onClick={() => underlineFunc(2)} variant="body1" sx={{ fontWeight: 'medium' }}>ABOUT</Typography>
+          </Link>
+        </Box>
       </Container>
     </Paper>
   );

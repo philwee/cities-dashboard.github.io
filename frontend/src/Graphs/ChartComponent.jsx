0c135c99721b7b4db355be3bd51bd7ca70cb81a4
princeampofo <pla252@nyu.edu>
2023-01-04 00:30:44 +0400

    fixed re-render issue

--- frontend/src/Graphs/ChartComponent.jsx
+++ frontend/src/Graphs/ChartComponent.jsx
@@ -1,10 +1,9 @@
 // disable eslint for this file
 /* eslint-disable */
-import { React, useState } from 'react'
+import { React, useState, useMemo, useCallback } from 'react';
 import { Chart } from 'react-google-charts';
 import { Box, CircularProgress, Tabs, Tab } from '@mui/material/';
 import HeatMap from './HeatMap';
-import parse from 'html-react-parser';
 
 import './ChartComponent.css';
 
@@ -19,21 +18,21 @@ function InnerChart({ chartData, chartIndex }) {
       format: chartData.vAxisFormat ?? 'decimal',
       title: chartData.vAxisTitle,
       viewWindow: {
-        min: chartData.vAxisMin
+        min: chartData.vAxisMin,
       },
       titleTextStyle: {
-        italic: false
-      }
+        italic: false,
+      },
     },
     hAxis: {
       format: chartData.hAxisFormat,
       title: chartData.hAxisTitle,
       viewWindow: {
-        min: chartData.hAxisMin
+        min: chartData.hAxisMin,
       },
       titleTextStyle: {
-        italic: false
-      }
+        italic: false,
+      },
     },
     width: '100%',
     height: '100%',
@@ -51,7 +50,7 @@ function InnerChart({ chartData, chartIndex }) {
       isHtml: true,
     },
     calendar: {
-      cellSize: scaleCalendar(5, 20) // calculate cell size for calendar chart
+      cellSize: scaleCalendar(5, 20), // calculate cell size for calendar chart
     },
     noDataPattern: {
       backgroundColor: 'none',
@@ -60,7 +59,7 @@ function InnerChart({ chartData, chartIndex }) {
     enableInteractivity: chartData.enableInteractivity,
     pointSize: chartData.pointSize,
     trendlines: chartData.trendlines,
-    region: chartData.region
+    region: chartData.region,
   };
 
   const chartEvents = [
@@ -83,8 +82,21 @@ function InnerChart({ chartData, chartIndex }) {
       className={chartData.chartType}
       marginLeft={chartData.chartType == 'Calendar' ? '-1rem' : ''}
       height={'100%'}
-      width={chartData.chartType == 'Calendar' ? '100vw' : '100%'}>
-      {circleProgress && <CircularProgress sx={{ display: 'block', position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, margin: 'auto' }} />}
+      width={chartData.chartType == 'Calendar' ? '100vw' : '100%'}
+    >
+      {circleProgress && (
+        <CircularProgress
+          sx={{
+            display: 'block',
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            margin: 'auto',
+          }}
+        />
+      )}
       <Chart
         chartType={chartData.chartType}
         chartWrapperParams={{
@@ -93,11 +105,14 @@ function InnerChart({ chartData, chartIndex }) {
         spreadSheetUrl={`https://docs.google.com/spreadsheets/d/${chartData.sheetId}`}
         spreadSheetQueryParameters={{
           headers: chartData.headers,
-          gid: Array.isArray(chartData.gid) ? chartData.gid[chartIndex] : chartData.gid,
+          gid: Array.isArray(chartData.gid)
+            ? chartData.gid[chartIndex]
+            : chartData.gid,
           query: chartData.query,
         }}
         options={options}
         chartEvents={chartEvents}
+        width={'100%'}
       />
     </Box>
   );
@@ -106,22 +121,22 @@ function InnerChart({ chartData, chartIndex }) {
 export default function ChartComponent({ chartData }) {
   // Case HeatMap
   if (chartData.chartType == 'HeatMap') {
-    return (
-      <HeatMap
-        chartData={chartData}
-      />
-    );
+    return <HeatMap chartData={chartData} />;
   }
 
   // Case WebsiteEmbed
   else if (chartData.chartType == 'WebsiteEmbed') {
     return (
-      <iframe src="https://nyuadair.com/historical#historical-graph-wrapper" width="100%" height="100%"></iframe>
+      <iframe
+        src="https://nyuadair.com/historical#historical-graph-wrapper"
+        width="100%"
+        height="100%"
+      ></iframe>
     );
   }
 
   // All the other chart types using React-Google-Chart wrapper
-  // If there are multiple charts 
+  // If there are multiple charts
   if (Array.isArray(chartData.gid)) {
     const array = chartData.gid;
 
@@ -143,31 +158,29 @@ export default function ChartComponent({ chartData }) {
           sx={{ mb: 2 }}
         >
           {array.map((element, index) => (
-            <Tab
-              key={index}
-              value={index}
-              label={array[index]}
-            />
+            <Tab key={index} value={index} label={array[index]} />
           ))}
         </Tabs>
-        <Box height="100%">
+        <Box position="relative" height="100%">
           {array.map((element, index) => (
             <Box
-            key={index}
-            height="100%"
-            role="tabpanel"
-            hidden={indexValue !== index}
-          >
-              <InnerChart chartData={chartData} chartIndex={index}/>
+              key={index}
+              height="100%"
+              width="100%"
+              role="tabpanel"
+              paddingBottom={4}
+              position="absolute"
+              top={0}
+              left={0}
+              visibility={indexValue === index ? 'visible' : 'hidden'}
+            >
+              <InnerChart chartData={chartData} chartIndex={index} />
             </Box>
           ))}
         </Box>
       </Box>
-
     );
   }
   // If there is only one single chart
-  else return (
-    <InnerChart chartData={chartData}/>
-  );
+  else return <InnerChart chartData={chartData} />;
 }

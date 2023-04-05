32324f51fd380042ca5f65dd2b23b265d3793014
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2023-04-05 20:23:39 +0700

    Updates

--- static/js/856.2424f115.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfront_end=self.webpackChunkfront_end||[]).push([[856],{6856:function(t,n,h){h.d(n,{$l_:function(){return a}});var c=h(9983);function a(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"}}]})(t)}}}]);
-//# sourceMappingURL=856.2424f115.chunk.js.map

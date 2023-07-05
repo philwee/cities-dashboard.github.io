b054e2612dad9bf2c225e8d9c2fbd7a668f2ddff
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2023-07-05 11:05:46 +0700

    Update FullWidthBox.jsx

--- frontend/src/Components/FullWidthBox.jsx
+++ frontend/src/Components/FullWidthBox.jsx
@@ -1,9 +1,9 @@
 import { styled } from '@mui/material/styles';
 import { Box } from '@mui/material';
 
-const FullWidthBox = styled(Box)(({ theme }) => ({
+const FullWidthBox = styled(Box)({
   paddingLeft: 'env(safe-area-inset-left)',
   paddingRight: 'env(safe-area-inset-right)',
-}));
+});
 
 export default FullWidthBox;

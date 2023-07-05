4056234c3182f7eff4fab292b329ebc4d33a6b7c
Ritin Malhotra <rm5486@nyu.edu>
2023-07-05 20:20:14 +0530

    Update `SheetsDataContext.jsx` With ESLint Changes

--- frontend/src/ContextProviders/SheetsDataContext.jsx
+++ frontend/src/ContextProviders/SheetsDataContext.jsx
@@ -1,66 +1,62 @@
-// disable eslint for this file
-/* eslint-disable */
-
-import { useState, useEffect, createContext } from "react";
-import { gapi } from "gapi-script";
+import { useState, useEffect, useCallback, createContext, useMemo } from 'react';
+import { gapi } from 'gapi-script';
 
 export const SheetsDataContext = createContext();
 
-export const SheetsDataProvider = (props) => {
-    const [sheetData, setSheetData] = useState({});
+export function SheetsDataProvider(props) {
+  const { children } = props;
 
-    useEffect(() => {
-        gapi.load("client:auth2", () => {
-            gapi.client.init({
-                apiKey: "AIzaSyCjVRS9swFZFN8FQq9ChM0FHWb_kRc0LCI",
-                discoveryDocs: [
-                    "https://sheets.googleapis.com/$discovery/rest?version=v4",
-                ],
-            }).then(() => {
-                getLastUpdateData().then((data) => {
-                    setSheetData(data);
-                });
-            }).catch(err => {
-                console.error(err);
-            });
-        });
-    }, []);
+  const [sheetData, setSheetData] = useState({});
+  const value = useMemo(() => [sheetData], [sheetData]);
 
-    const getLastUpdateData = async () => {
-        let sheetDataMap = {};
-        try {
-            const spreadsheetId = "1ddbsEukjKPX3tApu2nYRDoQpLMGpqFdrZ9jh_JYh5Tk";
-            const response = await gapi.client.sheets.spreadsheets.get({ spreadsheetId });
+  const getSheetNameFromRange = useCallback((range) => range.split('!')[0], []); // Range format: [sheetName]![A2:A2]
 
-            // Create an array of ranges
-            const ranges = response.result.sheets.map(sheet => `${sheet.properties.title}!A2:A2`);
-            // Use batchGet to get all ranges in a single Google Sheet document
-            // Avoid calling get multiple times --> Only 1 API call
-            const dataResponse = await gapi.client.sheets.spreadsheets.values.batchGet({
-                spreadsheetId,
-                ranges,
-            });
+  const getLastUpdateData = useCallback(async () => {
+    const sheetDataMap = {};
+    try {
+      const spreadsheetId = '1ddbsEukjKPX3tApu2nYRDoQpLMGpqFdrZ9jh_JYh5Tk';
+      const response = await gapi.client.sheets.spreadsheets.get({ spreadsheetId });
 
-            dataResponse.result.valueRanges.forEach((range, index) => {
-                if (range.values && range.values.length > 0) {
-                    const sheetName = getSheetNameFromRange(ranges[index]);
-                    sheetDataMap[sheetName] = range.values[0][0];
-                }
-            });
-            return sheetDataMap;
-        } catch (err) {
-            console.error(err);
-            return sheetDataMap;
+      const ranges = response.result.sheets.map((sheet) => `${sheet.properties.title}!A2:A2`);
+      const dataResponse = await gapi.client.sheets.spreadsheets.values.batchGet({
+        spreadsheetId,
+        ranges,
+      });
+
+      dataResponse.result.valueRanges.forEach((range, index) => {
+        if (range.values && range.values.length > 0) {
+          const sheetName = getSheetNameFromRange(ranges[index]);
+          const [[firstValue]] = range.values;
+          sheetDataMap[sheetName] = firstValue;
         }
-    };
+      });
+      return sheetDataMap;
+    } catch (err) {
+      console.error(err);
+      return sheetDataMap;
+    }
+  }, [getSheetNameFromRange]);
 
-    const getSheetNameFromRange = (range) => {
-        return range.split('!')[0]; // Range format: [sheetName]![A2:A2] 
-    };
+  useEffect(() => {
+    gapi.load('client:auth2', () => {
+      gapi.client.init({
+        apiKey: 'AIzaSyCjVRS9swFZFN8FQq9ChM0FHWb_kRc0LCI',
+        discoveryDocs: [
+          'https://sheets.googleapis.com/$discovery/rest?version=v4',
+        ],
+      }).then(() => {
+        getLastUpdateData().then((data) => {
+          setSheetData(data);
+        });
+      }).catch((err) => {
+        console.error(err);
+      });
+    });
+  }, [getLastUpdateData]);
 
-    return (
-        <SheetsDataContext.Provider value={[sheetData]}>
-            {props.children}
-        </SheetsDataContext.Provider>
-    );
-};
+  return (
+    <SheetsDataContext.Provider value={value}>
+      {children}
+    </SheetsDataContext.Provider>
+  );
+}

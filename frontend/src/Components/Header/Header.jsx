04e53066bc7300893ff3174ae6dd2fa2bec5654c
Ritin Malhotra <rm5486@nyu.edu>
2023-07-07 04:32:03 +0530

    Make Remaining `Components` ESLint Compliant

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,47 +1,40 @@
-// disable eslint for this file
-/* eslint-disable */
 import { useState, useContext } from 'react';
 import { styled } from '@mui/material/styles';
-import { LinkContext } from '../../ContextProviders/LinkContext';
 import { Tooltip, Box, Typography, Container, Paper, AppBar, Toolbar, useScrollTrigger, Slide, Stack, Drawer, Divider } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
+import parse from 'html-react-parser';
+import { LinkContext } from '../../ContextProviders/LinkContext';
 
 import ThemeSelector from './ThemeSelector';
 import NavBar from './NavBar';
 import SpeedDialButton from './SpeedDialButton';
 
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import SettingsIcon from '@mui/icons-material/Settings';
-
 // import images
 
 import jsonData from '../../section_data.json';
-import parse from 'html-react-parser';
 import { replacePlainHTMLWithMuiComponents } from '../../Utils/Utils';
-import { CITIESlogoLinkToHome } from './CITIESlogoLinkToHome';
+import CITIESlogoLinkToHome from './CITIESlogoLinkToHome';
 
-export const showInMobile = (defaultDisplay) => {
-  return { display: { xs: (defaultDisplay || "block"), lg: "none" } };
-}
-export const showInDesktop = (defaultDisplay) => {
-  return { display: { xs: "none", lg: (defaultDisplay || "block") } };
-}
+export const showInMobile = (defaultDisplay) => ({ display: { xs: (defaultDisplay || 'block'), lg: 'none' } });
+export const showInDesktop = (defaultDisplay) => ({ display: { xs: 'none', lg: (defaultDisplay || 'block') } });
 
 const toolBarHeightInRem = 3;
-const topAnchorID = "top-anchor";
+const topAnchorID = 'top-anchor';
 
 const StyledAppBar = styled(AppBar)(() => ({
   boxShadow: 'none',
-  "& .MuiToolbar-root": {
+  '& .MuiToolbar-root': {
     padding: 0
   }
 }));
 
 const StyledDrawer = styled(Drawer)(({ theme }) => ({
-  boxSizing: "border-box",
-  "& .MuiPaper-root": {
-    height: "auto",
-    borderRadius: "0.5rem",
+  boxSizing: 'border-box',
+  '& .MuiPaper-root': {
+    height: 'auto',
+    borderRadius: '0.5rem',
     margin: theme.spacing(2)
   }
 }));
@@ -49,6 +42,7 @@ const StyledDrawer = styled(Drawer)(({ theme }) => ({
 export default function Header(props) {
   const { setThemePreference } = props;
 
+  // eslint-disable-next-line no-unused-vars
   const [currentPage, _, chartsTitlesList, __] = useContext(LinkContext);
 
   // trigger for hiding/showing the AppBar
@@ -70,50 +64,51 @@ export default function Header(props) {
       <Slide appear={false} direction="down" in={!triggerHideAppBar}>
         <StyledAppBar enableColorOnDark component="nav">
           <Toolbar sx={{ backgroundColor: 'primary', height: `${toolBarHeightInRem}rem` }}>
-            <Container sx={{ height: "100%" }} >
+            <Container sx={{ height: '100%' }}>
               {/* CITIES logo and navbar */}
-              <Stack direction="row" justifyContent='space-between' alignItems='center' height="100%">
-                <Paper elevation={4}
+              <Stack direction="row" justifyContent="space-between" alignItems="center" height="100%">
+                <Paper
+                  elevation={4}
                   sx={{
-                    height: `${toolBarHeightInRem * 4 / 3}rem`,
+                    height: `${(toolBarHeightInRem * 4) / 3}rem`,
                     mt: `${toolBarHeightInRem}rem`,
                     opacity: triggerHideAppBar ? 0 : 1,
-                    borderRadius: "0.5rem",
-                    transition: '0.2s ease-in-out', '&:hover': { transform: 'scale(1.1)' },
+                    borderRadius: '0.5rem',
+                    transition: '0.2s ease-in-out',
+                    '&:hover': { transform: 'scale(1.1)' },
                   }}
                 >
                   <CITIESlogoLinkToHome />
                 </Paper>
 
-                <Stack direction="row" alignItems='center' justifyContent="flex-end" height="100%" spacing={2}>
+                <Stack direction="row" alignItems="center" justifyContent="flex-end" height="100%" spacing={2}>
 
                   {/* Navbar in landscape placed here, will be hidden in mobile  */}
-                  <Box sx={{ ...showInDesktop("block"), height: "100%" }}>
+                  <Box sx={{ ...showInDesktop('block'), height: '100%' }}>
                     <NavBar currentPage={currentPage} />
                   </Box>
 
-                  <Tooltip title={"Navigation Menu"}>
+                  <Tooltip title="Navigation Menu">
                     <IconButton
                       color="inherit"
                       aria-label="open mobile menu drawer"
                       edge="start"
                       onClick={handleDrawerToggle}
-                      sx={showInMobile("flex")}
+                      sx={showInMobile('flex')}
                     >
-                      <MenuIcon sx={{ fontSize: "1.25rem" }} />
+                      <MenuIcon sx={{ fontSize: '1.25rem' }} />
                     </IconButton>
                   </Tooltip>
 
-
-                  <Tooltip title={"Settings"}>
+                  <Tooltip title="Settings">
                     <IconButton
                       color="inherit"
                       aria-label="open setting drawer"
                       edge="start"
                       onClick={handleDrawerToggle}
-                      sx={showInDesktop("flex")}
+                      sx={showInDesktop('flex')}
                     >
-                      <SettingsIcon sx={{ fontSize: "1.25rem" }} />
+                      <SettingsIcon sx={{ fontSize: '1.25rem' }} />
                     </IconButton>
                   </Tooltip>
                 </Stack>
@@ -122,11 +117,11 @@ export default function Header(props) {
             </Container>
           </Toolbar>
         </StyledAppBar>
-      </Slide >
+      </Slide>
 
       <Box>
         <StyledDrawer
-          anchor="right" //from which side the drawer slides in
+          anchor="right" // from which side the drawer slides in
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
@@ -136,9 +131,9 @@ export default function Header(props) {
         >
           <Stack onClick={handleDrawerToggle}>
             {/* // Only show the NavBar here in mobile  */}
-            <Box sx={showInMobile("block")}>
+            <Box sx={showInMobile('block')}>
               <Container sx={{ py: 2 }}>
-                <Typography variant="h6" color="text.secondary" fontWeight='medium' gutterBottom>
+                <Typography variant="h6" color="text.secondary" fontWeight="medium" gutterBottom>
                   CITIES Dashboard
                 </Typography>
                 <NavBar currentPage={currentPage} />
@@ -147,35 +142,41 @@ export default function Header(props) {
             </Box>
 
             <Container sx={{ pt: 2, pb: 3 }}>
-              <Typography variant="h6" color="text.secondary" fontWeight='medium' gutterBottom>
+              <Typography variant="h6" color="text.secondary" fontWeight="medium" gutterBottom>
                 Dashboard Settings
               </Typography>
-              <ThemeSelector isFullWidth={true} setThemePreference={setThemePreference} />
+              <ThemeSelector isFullWidth setThemePreference={setThemePreference} />
             </Container>
 
           </Stack>
         </StyledDrawer>
-      </Box >
+      </Box>
 
-      {/* From MUI's documentation: 
-      When you render the app bar position fixed, the dimension of the element doesn't impact the rest of the page. This can cause some part of your content to be invisible, behind the app bar. Here is how to fix:
+      {/* From MUI's documentation:
+      When you render the app bar position fixed,
+      the dimension of the element doesn't impact the rest of the page.
+      This can cause some part of your content to be invisible,
+      behind the app bar. Here is how to fix:
       You can render a second <Toolbar /> component: */}
-      <Toolbar id={topAnchorID} sx={{ backgroundColor: "customAlternateBackground", height: `${toolBarHeightInRem * 1.5}rem` }
-      } />
+      <Toolbar
+        id={topAnchorID}
+        sx={{ backgroundColor: 'customAlternateBackground', height: `${toolBarHeightInRem * 1.5}rem` }}
+      />
 
-      <Box sx={{ 
+      <Box sx={{
         width: '100%',
-        paddingLeft: `env(safe-area-inset-left)`,
-        paddingRight: `env(safe-area-inset-right)`,
-        pt: 4, 
-        pb: 3, 
-        backgroundColor: "customAlternateBackground" 
-      }}>
-        <Container >
+        paddingLeft: 'env(safe-area-inset-left)',
+        paddingRight: 'env(safe-area-inset-right)',
+        pt: 4,
+        pb: 3,
+        backgroundColor: 'customAlternateBackground'
+      }}
+      >
+        <Container>
           <Typography
             variant="h3"
             color="text.primary"
-            fontWeight='medium'
+            fontWeight="medium"
           >
             CITIES DASHBOARD
           </Typography>
@@ -186,11 +187,10 @@ export default function Header(props) {
             })}
           </Typography>
         </Container>
-      </Box >
+      </Box>
 
       <SpeedDialButton chartsTitlesList={chartsTitlesList} topAnchorID={topAnchorID} />
 
     </>
   );
 }
-

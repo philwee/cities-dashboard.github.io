ef418eb049c1569859c752e4b478bfc0c24be5d6
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2023-08-04 12:43:35 +0700

    added # to hex colors

--- frontend/src/Themes/CustomThemes.jsx
+++ frontend/src/Themes/CustomThemes.jsx
@@ -1,26 +1,25 @@
 /* eslint-disable max-len */
 import { colors } from '@mui/material';
-import { dark, light } from '@mui/material/styles/createPalette';
 
 const darkShade = 400;
 const lightShade = 600;
 const darkShadeColorAxis = 300;
 
 const maroon = {
-  50: 'f0e0e5',
-  100: 'd8b3bd',
-  200: 'bf8091',
-  300: 'a54d65',
-  400: '912644',
-  500: '7e0023',
-  600: '76001f',
-  700: '6b001a',
-  800: '610015',
-  900: '4e000c',
-  A100: 'ff8189',
-  A200: 'ff4e5a',
-  A400: 'ff1b2a',
-  A700: 'ff0212'
+  50: '#f0e0e5',
+  100: '#d8b3bd',
+  200: '#bf8091',
+  300: '#a54d65',
+  400: '#912644',
+  500: '#7e0023',
+  600: '#76001f',
+  700: '#6b001a',
+  800: '#610015',
+  900: '#4e000c',
+  A100: '#ff8189',
+  A200: '#ff4e5a',
+  A400: '#ff1b2a',
+  A700: '#ff0212'
 };
 
 const getAQIPalette = ({ increasingOrder, isDark }) => {

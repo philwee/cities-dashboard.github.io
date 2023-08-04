056503b90fd2346ad75cfe3c2ce21b9b8754a8ff
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2023-08-04 15:28:14 +0700

    Revert "merge from Ritin branch"
    This reverts commit 640c6ead959f30fcb94c39d073fdd61308c12665.

---
+++ frontend/src/Components/Header/NavLinkBehavior.jsx
@@ -0,0 +1,6 @@
+const NavLinkBehavior = {
+  toNewPage: 'toNewPage',
+  scrollTo: 'scrollTo',
+};
+
+export default NavLinkBehavior;
--- frontend/src/Components/Header/SpeedDialButton.jsx
+++ frontend/src/Components/Header/SpeedDialButton.jsx
@@ -1,144 +1,123 @@
-// disable eslint for this file
-/* eslint-disable */
+/* eslint-disable react/jsx-props-no-spreading */
 import { Box, useScrollTrigger, Fab, Fade, Tooltip } from '@mui/material';
-
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
-
 import HoverMenu from 'material-ui-popup-state/HoverMenu';
-import PopupState, {
-    bindHover,
-    bindFocus,
-    bindMenu,
-} from 'material-ui-popup-state';
-import MenuItemAsNavLink from './MenuItemAsNavLink';
-import { StyledMenuItem } from './MenuItemAsNavLink';
-import { scrollToSection } from './MenuItemAsNavLink';
-import { NavLinkBehavior } from './NavBar';
-
+import PopupState, { bindHover, bindFocus, bindMenu } from 'material-ui-popup-state';
+import MenuItemAsNavLink, { StyledMenuItem, scrollToSection } from './MenuItemAsNavLink';
+import NavLinkBehavior from './NavLinkBehavior';
 import * as Tracking from '../../Utils/Tracking';
 
-const innerHeight = window.innerHeight;
+const { innerHeight } = window;
 
-function FadeInButton(props) {
-    const { children, window } = props;
+function FadeInButton({ children, window }) {
+  const trigger = useScrollTrigger({
+    target: window,
+    disableHysteresis: true,
+    threshold: innerHeight / 2,
+  });
 
-    const trigger = useScrollTrigger({
-        target: window,
-        disableHysteresis: true,
-        threshold: innerHeight / 2,
-    });
-
-    return (
-        <Fade in={trigger}>
-            <Box
-                role="presentation"
-                sx={{ position: 'fixed', bottom: "1rem", left: "calc(100vw - 5rem)" }}
-            >
-                {children}
-            </Box>
-        </Fade>
-    );
+  return (
+    <Fade in={trigger}>
+      <Box
+        role="presentation"
+        sx={{ position: 'fixed', bottom: '1rem', left: 'calc(100vw - 5rem)' }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
 }
 
-const HoverFocusMenu = (props) => {
-    const { popupId, label, menuItems } = props;
+function HoverFocusMenu({ popupId, label, menuItems }) {
+  return (
+    <PopupState variant="popover" popupId={popupId || 'demoPopup'}>
+      {(popupState) => (
+        <>
+          <Box {...bindHover(popupState)} {...bindFocus(popupState)}>
+            {label}
+          </Box>
+          <HoverMenu
+            {...bindMenu(popupState)}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            transformOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            onClick={popupState.close}
+          >
+            {menuItems}
+          </HoverMenu>
+        </>
+      )}
+    </PopupState>
+  );
+}
 
-    return (
-        <PopupState variant="popover" popupId={popupId || "demoPopup"}>
-            {(popupState) => (
-                <>
-                    <Box {...bindHover(popupState)} {...bindFocus(popupState)}>
-                        {label}
-                    </Box>
-                    {
-                        <HoverMenu
-                            {...bindMenu(popupState)}
-                            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-                            transformOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                            onClick={popupState.close}
-                        >
-                            {menuItems}
-                        </HoverMenu>
-                    }
-                </>
+function SpeedDialButton({ chartsTitlesList, topAnchorID }) {
+  const menuItemsArray = chartsTitlesList.length > 0 ? chartsTitlesList.map((element, index) => (
+    <MenuItemAsNavLink
+      key={index}
+      behavior={NavLinkBehavior.scrollTo}
+      scrollToSectionID={element.chartID}
+      label={`${element.chartID}. ${element.chartTitle}`}
+      analyticsOriginID="speed-dial"
+      analyticsDestinationLabel={element.chartTitle}
+      sx={{ fontSize: '0.8rem' }}
+    />
+  )) : [];
+
+  if (menuItemsArray.length > 0) {
+    menuItemsArray.unshift(
+      <StyledMenuItem disabled sx={{ fontWeight: '600' }}>
+        Quick Chart Navigation
+      </StyledMenuItem>,
+      <MenuItemAsNavLink
+        behavior={NavLinkBehavior.scrollTo}
+        scrollToSectionID={topAnchorID}
+        label="↑ Scroll to Top"
+        analyticsOriginID="speed-dial"
+        sx={{ fontSize: '0.8rem' }}
+      />
+    );
+  }
+
+  return (
+    <Box sx={{ zIndex: 99999999999999 }}>
+      <FadeInButton>
+        {chartsTitlesList.length > 0 ? (
+          <HoverFocusMenu
+            popupId="chart-list-popup"
+            label={(
+              <Fab sx={{ mt: 1 }} aria-label="popup menu" color="primary">
+                <FormatListNumberedIcon />
+              </Fab>
             )}
-        </PopupState>
-    );
-};
-
-export default function SpeedDialButton(props) {
-    const { chartsTitlesList, topAnchorID } = props;
-
-    let menuItemsArray = [];
-
-    if (chartsTitlesList.length > 0) {
-        menuItemsArray = chartsTitlesList.map((element, index) => (
-            <MenuItemAsNavLink
-                key={index}
-                behavior={NavLinkBehavior.scrollTo}
-                scrollToSectionID={element.chartID}
-                label={`${element.chartID}. ${element.chartTitle}`}
-                analyticsOriginID="speed-dial"
-                analyticsDestinationLabel={element.chartTitle}
-                sx={{ fontSize: "0.8rem" }}
-            />
-        ));
-
-        menuItemsArray.unshift([
-            <StyledMenuItem disabled sx={{ fontWeight: "600" }}>
-                Quick Chart Navigation
-            </StyledMenuItem>
-            ,
-            <MenuItemAsNavLink
-                behavior={NavLinkBehavior.scrollTo}
-                scrollToSectionID={topAnchorID}
-                label={'↑ Scroll to Top'}
-                analyticsOriginID="speed-dial"
-                sx={{ fontSize: "0.8rem" }}
-            />
-        ]
-        );
-    }
-
-    return (
-        <Box sx={{ zIndex: 99999999999999 }}>
-            <FadeInButton {...props}>
-                {
-                    // If there are charts to scroll to
-                    // display the list of charts' titles (below Scroll to top)
-                    chartsTitlesList.length > 0 ?
-                        <HoverFocusMenu
-                            popupId={"chart-list-popup"}
-                            label={
-                                <Fab sx={{ mt: 1 }} aria-label="popup menu" color="primary">
-                                    <FormatListNumberedIcon />
-                                </Fab>
-                            }
-                            menuItems={menuItemsArray}
-                        />
-                        :
-                        // If there is no chart to scroll to
-                        // Simply display a button to scroll to the top
-                        <Tooltip title="Scroll to Top">
-                            <Fab
-                                onClick={() => {
-                                    topAnchorID && scrollToSection(topAnchorID);
-                                    Tracking.sendEventAnalytics(Tracking.Events.internalNavigation,
-                                        {
-                                            destination_id: topAnchorID,
-                                            origin_id: "speed-dial"
-                                        });
-                                }}
-                                sx={{ mt: 1 }} aria-label="popup menu" color="primary"
-                            >
-                                <KeyboardArrowUpIcon />
-                            </Fab>
-                        </Tooltip>
-
+            menuItems={menuItemsArray}
+          />
+        ) : (
+          <Tooltip title="Scroll to Top">
+            <Fab
+              onClick={() => {
+                if (topAnchorID) {
+                  scrollToSection(topAnchorID);
                 }
-
-            </FadeInButton>
-        </Box>
-    );
+                Tracking.sendEventAnalytics(
+                  Tracking.Events.internalNavigation,
+                  {
+                    destination_id: topAnchorID,
+                    origin_id: 'speed-dial',
+                  },
+                );
+              }}
+              sx={{ mt: 1 }}
+              aria-label="popup menu"
+              color="primary"
+            >
+              <KeyboardArrowUpIcon />
+            </Fab>
+          </Tooltip>
+        )}
+      </FadeInButton>
+    </Box>
+  );
 }
+
+export default SpeedDialButton;

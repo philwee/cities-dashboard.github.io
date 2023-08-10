082ee672b3060ef8f11a847d9fc5c78c5caa685b
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2023-08-10 11:03:42 +0700

    potentially more optimized, needs testing

--- frontend/src/Graphs/ResponsiveCalendarChart.jsx
+++ frontend/src/Graphs/ResponsiveCalendarChart.jsx
@@ -1,9 +1,8 @@
-import { useState, useRef, useEffect, useCallback } from 'react';
+import { useState, useRef, useEffect, useCallback, useMemo } from 'react';
 import { Box, CircularProgress, styled } from '@mui/material/';
 import MemoizedChart from './MemoizedChart';
 
 const StyledChartWrapper = styled(Box)({
-  // Override React Google Charts to make Calendar chart truly responsive
   '& > div, & > div > div ': {
     width: '100% !important'
   },
@@ -15,7 +14,7 @@ const StyledChartWrapper = styled(Box)({
 const calculateCalendarDimensions = ({ cellSizeMin, cellSizeMax }) => {
   const cellSize = Math.min(Math.max((window.innerWidth * 0.9) / 58, cellSizeMin), cellSizeMax);
   return {
-    chartWidth: cellSize * 56, // fixed ratio
+    chartWidth: cellSize * 56,
     cellSize,
     yearLabelFontSize: cellSize * 2
   };
@@ -28,18 +27,10 @@ function CalendarChart({ chartData, chartProps, isPortrait, showControl }) {
 
   const [circleProgress, displayCircleProgress] = useState(true);
 
-  /**
-   * State and ref to track if the component is visible in the viewport.
-   * Chart only renders when it is visible to prevent expensive rendering.
-   * Helps alleviate a performance-related issue while loading the page containing the chart.
-  */
   const [isVisible, setIsVisible] = useState(false);
   const componentRef = useRef(null);
 
   useEffect(() => {
-    /**
-     * JavaScript IntersectionObserver API used to detect when chart is visible on screen.
-    */
     const currentRef = componentRef.current;
     const observer = new IntersectionObserver(
       ([entry]) => {
@@ -49,7 +40,7 @@ function CalendarChart({ chartData, chartProps, isPortrait, showControl }) {
       },
       {
         rootMargin: '0px',
-        threshold: 0.1 // Low threshold to start rendering chart as soon as it is visible
+        threshold: 0.1
       }
     );
 
@@ -57,113 +48,102 @@ function CalendarChart({ chartData, chartProps, isPortrait, showControl }) {
       observer.observe(currentRef);
     }
 
-    /**
-     * Cleanup function to remove observer when component is unmounted.
-     */
     return () => {
       if (currentRef) {
         observer.unobserve(currentRef);
       }
     };
-  }, [componentRef]);
+  }, []);
+
+  const hasLegend = useMemo(() => chartProps.options.legend?.position !== 'none', [chartProps.options.legend?.position]);
 
   const calculateChartTotalHeight = useCallback(() => {
-    // early return if both values are not ready
     if (!chartHeight.current) return;
     if (showControl && !controlHeight.current) return;
-    const hasLegend = chartProps.options.legend?.position !== 'none';
 
     let calculatedHeight = chartHeight.current + (hasLegend ? 30 : 60);
     calculatedHeight += controlHeight.current;
 
     setChartTotalHeight(calculatedHeight);
-  }, [chartProps.options.legend?.position, showControl]);
+  }, [hasLegend, showControl]);
 
   const updateChartHeight = useCallback((chartWrapper) => {
-    // from the chartWrapper, querySelector is used to select the first 'g' element in the svg.
+    if (!chartWrapper || !chartWrapper.container) return;
     const chartContainer = chartWrapper.container.querySelector('svg > g:nth-of-type(1)');
     const renderedHeight = chartContainer.getBBox().height;
 
     chartHeight.current = renderedHeight;
-
     calculateChartTotalHeight();
   }, [calculateChartTotalHeight]);
 
   const updateControlHeight = useCallback((controlWrapper) => {
+    if (!controlWrapper || typeof controlWrapper.getControl !== 'function') return;
     const controlContainer = controlWrapper.getControl().container;
     const renderedHeight = controlContainer.getBoundingClientRect().height;
 
     controlHeight.current = renderedHeight;
-
     calculateChartTotalHeight();
   }, [calculateChartTotalHeight]);
 
   const calendarDimensions = calculateCalendarDimensions({ cellSizeMin: 10, cellSizeMax: 18 });
 
-  // When chart is ready
-  const chartEvents = [
+  const chartEvents = useMemo(() => [
     {
       eventName: 'ready',
-      callback: useCallback(({ chartWrapper }) => {
+      callback: ({ chartWrapper }) => {
         updateChartHeight(chartWrapper.getChart());
         displayCircleProgress(false);
-      }, [updateChartHeight])
+      }
     }
-  ];
+  ], [updateChartHeight]);
 
-  const controlEvents = [
+  const controlEvents = useMemo(() => [
     {
       eventName: 'ready',
-      callback: useCallback(({ controlWrapper }) => {
+      callback: ({ controlWrapper }) => {
         updateControlHeight(controlWrapper);
-      }, [updateControlHeight])
+      }
     },
     {
       eventName: 'statechange',
-      callback: useCallback(({ controlWrapper }) => {
+      callback: ({ controlWrapper }) => {
         updateControlHeight(controlWrapper);
-      }, [updateControlHeight]),
+      }
     },
-  ];
+  ], [updateControlHeight]);
 
-  const calendarChartProps = {
-    ...chartProps,
-    options: {
-      ...chartProps.options,
-      // overcompensate the height of chart SVG element. this is OK as
-      // the chart container will clip the chart to it's expected height of {chartTotalHeight}
-      height: '1000px',
-      width: calendarDimensions.chartWidth,
-      calendar: {
-        cellSize: calendarDimensions.cellSize,
-        yearLabel: {
-          fontSize: calendarDimensions.yearLabelFontSize
+  const calendarChartProps = useMemo(() => {
+    const props = {
+      ...chartProps,
+      options: {
+        ...chartProps.options,
+        height: '1000px',
+        width: calendarDimensions.chartWidth,
+        calendar: {
+          cellSize: calendarDimensions.cellSize,
+          yearLabel: {
+            fontSize: calendarDimensions.yearLabelFontSize
+          },
+          daysOfWeek: isPortrait ? '' : 'SMTWTFS'
         },
-        daysOfWeek: isPortrait ? '' : 'SMTWTFS' // hide dayOfWeek label on mobile to save space
+        noDataPattern: {
+          backgroundColor: 'none',
+          color: 'none',
+        },
       },
-      noDataPattern: {
-        backgroundColor: 'none',
-        color: 'none',
-      },
-    },
-    chartEvents
-  };
+      chartEvents
+    };
 
-  // additional props if there is a controlFilter present
-  if (showControl) {
-    calendarChartProps.controls = [{
-      ...calendarChartProps.controls[0],
-      controlEvents,
-    }];
-  }
+    if (showControl) {
+      props.controls = [{
+        ...props.controls[0],
+        controlEvents,
+      }];
+    }
 
-  /**
- * Render logic for the CalendarChart component.
- * If the component is not visible, a placeholder div is rendered for observation.
- * placeholder div needed to observe visibility since it contains the componentRef.
- * Once visible, the complete CalendarChart component is rendered.
- * @returns {JSX.Element} The CalendarChart component or a placeholder div.
- */
+    return props;
+  }, [chartProps, calendarDimensions, chartEvents, controlEvents, isPortrait, showControl]);
+
   return (
     isVisible ? (
       <StyledChartWrapper
